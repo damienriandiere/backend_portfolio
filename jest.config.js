9f263f0a7b4a9ea665358f0848cf53dc07958549
Kerpo <damien.riandiere@outlook.fr>
2024-04-15 15:56:50 +0200

    Add TU

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    testMatch: ["**/__tests__/**/*.js"],
+    testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: ["src/**/*.js"],
+    coverageDirectory: "coverage",
+    coverageReporters: ["text", "lcov"],
+    rootDir: "."
+};

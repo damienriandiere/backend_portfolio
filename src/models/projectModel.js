4ca8d693c01a1852662611bd657100389842b6ad
Kerpo <damien.riandiere@outlook.fr>
2024-04-17 21:32:57 +0200

    Update constraints about project model

--- src/models/projectModel.js
+++ src/models/projectModel.js
@@ -1,14 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const wordCountValidator = (maxWords) => {
+  return function (value) {
+    const wordCount = value.trim().split(/\s+/).length;
+    return wordCount <= maxWords;
+  };
+};
+
 const ProjectSchema = new Schema({
-	title: { type: String, required: true },
-	introductoryDescription: { type: String, maxlength: 80, required: true},
-	completeDescription: { type: String, maxlength: 1000, required: true},
-	keywords: { type: [String], maxlength: 10, required: true},
-	thumbnailImage: { type: String, required: true }, 
-	illustrationImages: { type: [String], maxlength: 5, required: true }, 
-  });
-const Project = mongoose.model('Projects', ProjectSchema);
+  title: { type: String, required: true, unique: true },
+  introductoryDescription: { type: String, maxlength: 80, required: true },
+  completeDescription: {
+    type: String,
+    validate: wordCountValidator(250),
+    required: true,
+  },
+  keywords: {
+    type: [String],
+    validate: {
+      validator: function (val) {
+        return val.length <= 10;
+      },
+      message: "Maximum 10 mots-clés autorisés",
+    },
+    required: true,
+  },
+  thumbnailImage: { type: String, required: true },
+  illustrationImages: {
+    type: [String],
+    validate: {
+      validator: function (val) {
+        return 1 <= val.length <= 5;
+      },
+      message: "Entre 1 et 5 images d'illustrations",
+    },
+    required: true,
+  },
+});
+const Project = mongoose.model("Projects", ProjectSchema);
 
 module.exports = Project;
